c6a19643f30e3601ca325b195f71576da9a28a62
Seybou <seybou@yahoo.com>
2019-12-29 06:31:49 +0000

    Quizz2 done

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,4 +2,20 @@ class CarsController < ApplicationController
      def index
         @car = Car.all
   end
+
+   def new
+    @car = Car.new
+  end
+
+    def create
+        Car.create(car_params)
+         redirect_to root_path
+  end
+
+ private
+
+  def car_params
+    params.require(:car).permit(:name, :description)
+  end
+
 end
